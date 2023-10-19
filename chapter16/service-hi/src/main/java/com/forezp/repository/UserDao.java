246bc3ddf6b8fe48529e8d255ed13f9640577728
yushusoft <qiuyuchun@163.com>
2023-10-19 17:57:45 +0800

    删除分支
    Signed-off-by: yushusoft <qiuyuchun@163.com>

--- chapter16/service-hi/src/main/java/com/forezp/repository/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.forezp.repository;
-
-import com.forezp.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-
-	User findByUsername(String username);
-}
